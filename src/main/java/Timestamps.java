af5b0fdc5dc559ae67a8b536ad134e5bf8751fe6
Lian James Modico <lianjam.157771@nv.ccsd.net>
2020-09-05 22:00:34 +0000

    I worked on Seconds Past Midnight and Timestamps.

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -13,11 +13,22 @@ class Timestamps {
         int hours2 = scanner.nextInt();
         int minutes2 = scanner.nextInt();
         int seconds2 = scanner.nextInt();
-
+        int secInHour1, secInHour2;
+        int secInMins1, secInMins2;
+        int total1, total2;
+        int diff;
 
         /*
          *  your code goes here
          */
+           secInHour1 = hours1 * 3600;
+           secInHour2 = hours2 * 3600;
+           secInMins1 = minutes1 * 60;
+           secInMins2 = minutes2 * 60;
+           total1 = secInHour1 + secInMins1 + seconds1;
+           total2 = secInHour2 + secInMins2 + seconds2;
+           diff = total2 - total1;
+           System.out.println(diff);
 
 
         // closing the scanner object
