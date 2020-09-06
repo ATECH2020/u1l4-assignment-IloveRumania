ab8d2ee6ee57d388308b2b5d1867abd6dbb2371d
Lian James Modico <lianjam.157771@nv.ccsd.net>
2020-09-06 00:28:21 +0000

    I worked on the Student Desks.

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,12 +7,19 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        int students1 = scanner.nextInt();
+        int students2 = scanner.nextInt();
+        int students3 = scanner.nextInt();
 
         /*
          *  your code goes here
          */
-
+         int desks1 = students1 / 2;
+         int desks2 = students2 / 2;
+         int desks3 = students3 / 2;
+         System.out.println(Math.round(desks1));
+         System.out.println(Math.round(desks2));
+         System.out.println(Math.round(desks3));
 
         // closing the scanner object
         scanner.close();
